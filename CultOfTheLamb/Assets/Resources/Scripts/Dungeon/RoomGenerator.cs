5400816b6af2b4a61437511526c23338e00c3855
Kimdeahun0824 <garamei72@gmail.com>
2023-02-25 19:13:45 +0900

    33. Commit [ Dev ]

--- CultOfTheLamb/Assets/Resources/Scripts/Dungeon/RoomGenerator.cs
+++ CultOfTheLamb/Assets/Resources/Scripts/Dungeon/RoomGenerator.cs
@@ -3,26 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 public enum RoomType
 {
-    NONE, START, NORMAL, BOSS, TAROT
+    NONE, START, NORMAL, BOSS, TAROT, ENDROOM
 }
 public class RoomGenerator : MonoBehaviour
 {
     public RoomNode[,] rooms = default;
-    public GameObject test;
+    public GameObject mapPrefab;
+    public Transform parent;
     int count;
     int tempCount;
+    int cost;
+
     void Start()
     {
         rooms = new RoomNode[5, 5];
+        count = 0;
         tempCount = 0;
-        //rooms[0, 0].currentRoomType = RoomType.NONE;
+        cost = 0;
         RoomCreate();
 
+        StartCoroutine(RoomDeployment());
+    }
+
+    IEnumerator RoomDeployment()
+    {
+        for (int i = 0; i < rooms.GetLength(0); i++)
+        {
+            for (int j = 0; j < rooms.GetLength(1); j++)
+            {
+                if (rooms[i, j].currentRoomType != RoomType.NONE)
+                {
+                    rooms[i, j].X = i;
+                    rooms[i, j].Y = j;
+                    //Instantiate(mapPrefab, new Vector3(AStarManager.Instance.aGrid.gridWorldSize.x * i, 0.1f, AStarManager.Instance.aGrid.gridWorldSize.y * j), Quaternion.identity, parent);
+                    //roomsObj[i, j] = Instantiate(mapPrefab, new Vector3(AStarManager.Instance.aGrid.gridWorldSize.x * i, 0.1f, AStarManager.Instance.aGrid.gridWorldSize.y * j), Quaternion.identity, parent);     
+                    Instantiate(mapPrefab, new Vector3(0, 0.1f, 0), Quaternion.identity, parent);
+                    yield return null;
+                }
+                Debug.Log($"room Status i : {i} / j : {j} / roomType : {rooms[i, j].currentRoomType} / roomCost : {rooms[i, j].Cost}");
+            }
+        }
 
     }
 
     public void RoomCreate()
     {
+        count = 0;
+        tempCount = 0;
+        cost = 0;
         for (int i = 0; i < rooms.GetLength(0); i++)
         {
             for (int j = 0; j < rooms.GetLength(1); j++)
@@ -31,23 +59,26 @@ public class RoomGenerator : MonoBehaviour
                 rooms[i, j].currentRoomType = RoomType.NONE;
             }
         }
+
         int x = Random.Range(0, 5);
         int y = Random.Range(0, 5);
         rooms[x, y].currentRoomType = RoomType.START;
+
         RandomRoomCreate(x, y);
+        RoomDFSFind(x, y, x, y);
+
         for (int i = 0; i < rooms.GetLength(0); i++)
         {
             for (int j = 0; j < rooms.GetLength(1); j++)
             {
-                Debug.Log($"room Status i:{i} j:{j} : {rooms[i, j].currentRoomType}");
                 if (rooms[i, j].currentRoomType != RoomType.NONE)
                 {
-                    Instantiate(test, new Vector3(i, 0, j), Quaternion.identity);
                     tempCount++;
                 }
             }
         }
-        if (tempCount < 8)
+
+        if (tempCount < 9)
         {
             RoomCreate();
         }
@@ -56,168 +87,176 @@ public class RoomGenerator : MonoBehaviour
     public void RandomRoomCreate(int x, int y)
     {
         if (8 <= count) return;
-        // int randX = Random.Range(0, 2);
-        // int randY = Random.Range(0, 2);
-        // if (randX == 0) randX = -1;
-        // if (randY == 0) randY = -1;
-        // int currentX = x + randX;
-        // int currentY = y + randY;
-        // if (currentX < 0 || 4 < currentX || currentY < 0 || 4 < currentY)
-        // {
-        //     RandomRoomCreate(x, y);
-        //     return;
-        // }
-        // if (rooms[currentX, currentY].currentRoomType == RoomType.NONE)
-        // {
-        //     rooms[currentX, currentY].currentRoomType = RoomType.NORMAL;
-        //     count++;
-        //     RandomRoomCreate(currentX, currentY);
-        // }
-        // if (0 < x - 1)
-        // {
-        //     if (rooms[x - 1, y].currentRoomType == RoomType.NONE)
-        //     {
-        //         int randNum = Random.Range(0, 2);
-        //         if (randNum == 1)
-        //         {
-        //             rooms[x - 1, y].currentRoomType = RoomType.NORMAL;
-        //             RandomRoomCreate(x - 1, y);
-        //             count++;
-        //         }
-        //     }
-        // }
-        // if (0 < x - 1 && y + 1 < 4)
-        // {
-        //     if (rooms[x - 1, y + 1].currentRoomType == RoomType.NONE)
-        //     {
-        //         int randNum = Random.Range(0, 2);
-        //         if (randNum == 1)
-        //         {
-        //             rooms[x - 1, y + 1].currentRoomType = RoomType.NORMAL;
-        //             RandomRoomCreate(x - 1, y + 1);
-        //             count++;
-        //         }
-        //     }
-        // }
-        // if (x + 1 < 4)
-        // {
-        //     if (rooms[x + 1, y].currentRoomType == RoomType.NONE)
-        //     {
-        //         int randNum = Random.Range(0, 2);
-        //         if (randNum == 1)
-        //         {
-        //             rooms[x + 1, y].currentRoomType = RoomType.NORMAL;
-        //             RandomRoomCreate(x + 1, y);
-        //             count++;
-        //         }
-        //     }
-        // }
-        // if (x + 1 < 4 && 0 < y - 1)
-        // {
-        //     if (rooms[x + 1, y - 1].currentRoomType == RoomType.NONE)
-        //     {
-        //         int randNum = Random.Range(0, 2);
-        //         if (randNum == 1)
-        //         {
-        //             rooms[x + 1, y - 1].currentRoomType = RoomType.NORMAL;
-        //             RandomRoomCreate(x + 1, y - 1);
-        //             count++;
-        //         }
-        //     }
-        // }
+
         int index = 0;
-        if (y + 1 < 4)
+        index = NodeNeighborCheck(x, y);
+        if (2 < index)
+        {
+            return;
+        }
+
+        if (y + 1 <= rooms.GetLength(1) - 1)
+        {
+            if (NodeNeighborCheck(x, y + 1) < 2 && count < 8)
+            {
+                if (rooms[x, y + 1].currentRoomType == RoomType.NONE)
+                {
+                    int randNum = Random.Range(0, 2);
+                    if (randNum == 1)
+                    {
+                        rooms[x, y + 1].currentRoomType = RoomType.NORMAL;
+                        count++;
+                        Debug.Log($"RoomCreate UP : i : {x} / j : {y + 1} / Count : {count}");
+                        RandomRoomCreate(x, y + 1);
+                    }
+                }
+            }
+        }
+        if (x + 1 <= rooms.GetLength(0) - 1)
+        {
+            if (NodeNeighborCheck(x + 1, y) < 2 && count < 8)
+            {
+                if (rooms[x + 1, y].currentRoomType == RoomType.NONE)
+                {
+                    int randNum = Random.Range(0, 2);
+                    if (randNum == 1)
+                    {
+                        rooms[x + 1, y].currentRoomType = RoomType.NORMAL;
+                        count++;
+                        Debug.Log($"RoomCreate RIGHT : i : {x + 1} / j : {y} / Count : {count}");
+                        RandomRoomCreate(x + 1, y);
+                    }
+                }
+            }
+        }
+        if (0 <= x - 1)
+        {
+            if (NodeNeighborCheck(x - 1, y) < 2 && count < 8)
+            {
+                if (rooms[x - 1, y].currentRoomType == RoomType.NONE)
+                {
+                    int randNum = Random.Range(0, 2);
+                    if (randNum == 1)
+                    {
+                        rooms[x - 1, y].currentRoomType = RoomType.NORMAL;
+                        count++;
+                        Debug.Log($"RoomCreate LEFT : i : {x - 1} / j : {y} / Count : {count}");
+                        RandomRoomCreate(x - 1, y);
+                    }
+                }
+            }
+        }
+        if (0 <= y - 1)
+        {
+            if (NodeNeighborCheck(x, y - 1) < 2 && count < 8)
+            {
+                if (rooms[x, y - 1].currentRoomType == RoomType.NONE)
+                {
+                    int randNum = Random.Range(0, 2);
+                    if (randNum == 1)
+                    {
+                        rooms[x, y - 1].currentRoomType = RoomType.NORMAL;
+                        count++;
+                        Debug.Log($"RoomCreate DOWN : i : {x} / j : {y - 1} / Count : {count}");
+                        RandomRoomCreate(x, y - 1);
+                    }
+                }
+            }
+        }
+    }
+
+    public int NodeNeighborCheck(int x, int y)
+    {
+        int index = 0;
+        if (y + 1 <= rooms.GetLength(1) - 1)
         {
             if (rooms[x, y + 1].currentRoomType != RoomType.NONE)
             {
                 index++;
             }
         }
-        if (x + 1 < 4)
+        if (x + 1 <= rooms.GetLength(0) - 1)
         {
             if (rooms[x + 1, y].currentRoomType != RoomType.NONE)
             {
                 index++;
             }
         }
-        if (0 < x - 1)
+        if (0 <= x - 1)
         {
             if (rooms[x - 1, y].currentRoomType != RoomType.NONE)
             {
                 index++;
             }
         }
-        if (0 < y - 1)
+        if (0 <= y - 1)
         {
             if (rooms[x, y - 1].currentRoomType != RoomType.NONE)
             {
                 index++;
             }
         }
-        if (2 < index)
-        {
-            return;
-        }
-        if (y + 1 <= 4)
-        {
-            if (rooms[x, y + 1].currentRoomType == RoomType.NONE)
-            {
-                int randNum = Random.Range(0, 2);
-                if (randNum == 1)
-                {
-                    rooms[x, y + 1].currentRoomType = RoomType.NORMAL;
-                    count++;
-                    RandomRoomCreate(x, y + 1);
-                }
-            }
-        }
-        if (x + 1 <= 4)
-        {
-            if (rooms[x + 1, y].currentRoomType == RoomType.NONE)
-            {
-                int randNum = Random.Range(0, 2);
-                if (randNum == 1)
-                {
-                    rooms[x + 1, y].currentRoomType = RoomType.NORMAL;
-                    count++;
-                    RandomRoomCreate(x + 1, y);
-                }
-            }
-        }
+
+        return index;
+    }
+
+    public void RoomDFSFind(int x, int y, int previousX, int previousY)
+    {
+        rooms[x, y].IsChecked = true;
+        rooms[x, y].Cost = rooms[previousX, previousY].Cost + 1;
+        bool endCheck = true;
+
         if (0 <= x - 1)
         {
-            if (rooms[x - 1, y].currentRoomType == RoomType.NONE)
+            if (rooms[x - 1, y].currentRoomType != RoomType.NONE && !rooms[x - 1, y].IsChecked)
             {
-                int randNum = Random.Range(0, 2);
-                if (randNum == 1)
-                {
-                    rooms[x - 1, y].currentRoomType = RoomType.NORMAL;
-                    count++;
-                    RandomRoomCreate(x - 1, y);
-                }
+                endCheck = false;
+                RoomDFSFind(x - 1, y, x, y);
             }
         }
+
+        if (y + 1 <= 4)
+        {
+
+            if (rooms[x, y + 1].currentRoomType != RoomType.NONE && !rooms[x, y + 1].IsChecked)
+            {
+                endCheck = false;
+                RoomDFSFind(x, y + 1, x, y);
+            }
+        }
+
+        if (x + 1 <= 4)
+        {
+
+            if (rooms[x + 1, y].currentRoomType != RoomType.NONE && !rooms[x + 1, y].IsChecked)
+            {
+                endCheck = false;
+                RoomDFSFind(x + 1, y, x, y);
+            }
+        }
+
         if (0 <= y - 1)
         {
-            if (rooms[x, y - 1].currentRoomType == RoomType.NONE)
+            if (rooms[x, y - 1].currentRoomType != RoomType.NONE && !rooms[x, y - 1].IsChecked)
             {
-                int randNum = Random.Range(0, 2);
-                if (randNum == 1)
-                {
-                    rooms[x, y - 1].currentRoomType = RoomType.NORMAL;
-                    count++;
-                    RandomRoomCreate(x, y - 1);
-                }
+                endCheck = false;
+                RoomDFSFind(x, y - 1, x, y);
             }
         }
 
-
+        if (endCheck)
+        {
+            rooms[x, y].currentRoomType = RoomType.ENDROOM;
+        }
     }
 }
 
 public class RoomNode
 {
     public RoomType currentRoomType = RoomType.NONE;
+    public int X = 0, Y = 0;
+    public int Cost = 0;
+    public bool IsChecked = default;
 }
 
 
